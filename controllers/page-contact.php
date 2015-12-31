4053dd77d1ce06fedba4099e7a7388d3c7fd066c
Jonjoe Whitfield <me@jonjoe.io>
2015-12-31 18:49:31 +0000

    asset ref bug

--- controllers/page-contact.php
+++ controllers/page-contact.php
@@ -10,6 +10,7 @@
 	$ctx = Timber::get_context();
 	// Add to the array
 	$ctx["post"] = Timber::get_post();
+	$ctx["theme"] = $themeGlobals;
 	// Render a view
 	Timber::render('contact.twig', $ctx);
 	// Get the footer content
